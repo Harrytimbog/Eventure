c0a947dc0341543ad9ff30c296bef032dfb2e62e
Timilehin Arigbede <harriemannie@gmail.com>
2024-12-18 01:18:21 +0000

    Setting up Axios

--- Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
+++ Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+418e7dfd7c13f549642bf859ac7bb48505bbd277")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8091f70bd860558a3610d508185f694947d0708")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
