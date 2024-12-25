bd876aeb669b4a916b71e426e4233111f3d033b9
Timilehin Arigbede <harriemannie@gmail.com>
2024-12-25 19:09:17 +0000

    Refactor the app to use MobX

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0dc49bbcb186fb33afb9d170f15f027aa231befd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec97080dca8696fea560fc5c1606ed488ee34aa3")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
