2ec0699f5ce33d8180e9cfb9a25ecf495bd481e1
Timilehin Arigbede <harriemannie@gmail.com>
2024-12-27 13:16:30 +0000

    Adding Details Link

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ebac08cdf04e554ead98b27a06dbfed00ffbe18")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1108eb3abb292e0a34a9b2f43f24a291c8a8b3a")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
