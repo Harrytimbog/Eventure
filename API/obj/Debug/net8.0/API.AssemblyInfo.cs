ebc3385084e28c869140946017cbcebb39c90d30
Timilehin Arigbede <harriemannie@gmail.com>
2025-01-01 18:18:53 +0000

    Populating the Detailed Components

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1971d37c2f817a37c6d315047b597dcfd612b7a8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f10cb6d528a3a641320edf0ab64d1454325caa8")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
