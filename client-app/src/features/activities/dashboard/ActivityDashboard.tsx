54cf2c621cf36fe9756aac69cb44953166bf1e7e
Timilehin Arigbede <harriemannie@gmail.com>
2025-01-01 19:38:42 +0000

    Adding the Activity Filter Component

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -4,6 +4,7 @@ import { useStore } from '../../../app/stores/store'
 import { observer } from 'mobx-react-lite'
 import { useEffect } from 'react'
 import LoadingComponent from '../../../app/layout/LoadingComponent'
+import ActivityFilters from './ActivityFilters'
 
 
 const ActivityDashboard = () => {
@@ -14,7 +15,7 @@ const ActivityDashboard = () => {
     useEffect(() => {
         if (activityRegistry.size <= 1) {
             loadActivities();
-        }   
+        }
     }, [loadActivities, activityRegistry.size]);
 
 
@@ -27,7 +28,7 @@ const ActivityDashboard = () => {
                     <ActivityList />
                 </Grid.Column>
                 <Grid.Column width='6'>
-                    <h2>Activity Filters</h2>
+                    <ActivityFilters />
                 </Grid.Column>
             </Grid>
         </>
