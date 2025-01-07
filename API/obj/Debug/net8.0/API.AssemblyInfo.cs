c35e29ba2b2b3236da544d3164411f4682e1cd41
timilehin arigbede <harriemannie@gmail.com>
2025-01-07 14:10:09 +0000

    Formik with less code

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0d7957dc9c7f5f680d753db9ffcadfb029becd1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9fcd354859077f08c1d2eb0d72e33bed110d56b")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
