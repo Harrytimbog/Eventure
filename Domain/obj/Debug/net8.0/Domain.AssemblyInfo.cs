f0d7957dc9c7f5f680d753db9ffcadfb029becd1
timilehin arigbede <harriemannie@gmail.com>
2025-01-07 13:26:24 +0000

    Client-side | Handling the  Validation Error from an Invalid GUID

--- Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6956d6045f5d1fcd7974ed4608a907653880af5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e270975d33aea6808c2af5e8f0d9778a82252ea5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
